40d80d19b033d2fd786c57227ef0d3121b1749d4
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-12 18:39:52 +0300

    feat: created navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import "./navbar.scss";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="left">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt="Netflix"
+          />
+          <span>Home Page</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>List</span>
+        </div>
+        <div className="right">
+          <SearchIcon className="icon" />
+          <span>Kıd</span>
+          <NotificationsIcon className="icon" />
+          <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt="Profile Picture"
+          />
+          <div className="profile">
+            <ArrowDropDownIcon className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Log out</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,7 +1,12 @@
 import "./home.scss";
+import Navbar from "../components/navbar/Navbar";
 
 function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Navbar />
+    </div>
+  );
 }
 
 export default Home;
