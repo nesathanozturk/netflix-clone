a68f55e930472beae31d87336d5d0c41aeb49861
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-12 20:16:51 +0300

    feat: created watch component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Home from "./pages/home/Home";
 import "./app.scss";
+import Watch from "./pages/watch/Watch";
 
 function App() {
-  return <Home />;
+  return <Watch />;
 }
 
 export default App;
