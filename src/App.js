9c90ff4cf050afdad57041f1f7af9849b9821f40
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-12 20:46:46 +0300

    feat: created login page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Home from "./pages/home/Home";
 import "./app.scss";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 function App() {
-  return <Register />;
+  return <Login />;
 }
 
 export default App;
