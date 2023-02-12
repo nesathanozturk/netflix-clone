0caa1da4c5668365cf85d3576a207f48a8d400db
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-12 19:39:56 +0300

    feat: created slider functions

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,15 +1,41 @@
+import { useState, useRef } from "react";
 import "./list.scss";
 import ListItem from "../listItem/ListItem";
 import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
 import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
 
 function List() {
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [isMoved, setIsMoved] = useState(false);
+
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlinedIcon />
-        <div className="container">
+        <ArrowBackIosOutlinedIcon
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem />
           <ListItem />
           <ListItem />
           <ListItem />
@@ -20,7 +46,10 @@ function List() {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowForwardIosOutlinedIcon />
+        <ArrowForwardIosOutlinedIcon
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
