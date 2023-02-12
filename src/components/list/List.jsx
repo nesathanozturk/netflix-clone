6a5ce91888a108ae5b9a4506280a50597aaea40c
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-12 19:16:08 +0300

    feat: created listItem component

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,7 +1,29 @@
 import "./list.scss";
+import ListItem from "../listItem/ListItem";
+import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
 
 function List() {
-  return <div className="list">lİST</div>;
+  return (
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlinedIcon />
+        <div className="container">
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        </div>
+        <ArrowForwardIosOutlinedIcon />
+      </div>
+    </div>
+  );
 }
 
 export default List;
---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,7 @@
+import "./listItem.scss";
+
+function listItem() {
+  return <div className="listItem">wq</div>;
+}
+
+export default listItem;
