f39e9e0506fe3514798b8704f6865fe0eecd09d3
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-12 22:58:48 +0300

    feat: added roboto font

--- src/components/FeaturedMovie/FeaturedMovie.jsx
+++ src/components/FeaturedMovie/FeaturedMovie.jsx
@@ -2,35 +2,14 @@ import "../../styles/featuredMovie.scss";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
-function FeaturedMovie({ type }) {
+function FeaturedMovie() {
   return (
-    <div className="featuredMovie">
-      {type && (
-        <div className="category">
-          <span>{type === "movie" ? "Movies" : "Series"}</span>
-          <select name="genre" id="genre">
-            <option>Genre</option>
-            <option value="adventure">Adventure</option>
-            <option value="comedy">Comedy</option>
-            <option value="crime">Crime</option>
-            <option value="fantasy">Fantasy</option>
-            <option value="historical">Historical</option>
-            <option value="horror">Horror</option>
-            <option value="romance">Romance</option>
-            <option value="sci-fi">Sci-fi</option>
-            <option value="thriller">Thriller</option>
-            <option value="western">Western</option>
-            <option value="animation">Animation</option>
-            <option value="drama">Drama</option>
-            <option value="documentary">Documentary</option>
-          </select>
-        </div>
-      )}
+    <div className="featured_movie">
       <img
-        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        src="https://www.teahub.io/photos/full/60-609425_peaky-blinders-wallpapers-group-peaky-blinders-wallpaper-4k.jpg"
         alt="Background Poster"
       />
-      <div className="movieInfo">
+      <div className="movie_info">
         <img
           src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
           alt="Movie Poster"
@@ -43,10 +22,10 @@ function FeaturedMovie({ type }) {
         </span>
         <div className="buttons">
           <button className="playBtn">
-            <PlayArrowIcon /> <span>Play</span>
+            <PlayArrowIcon /> <span>Oynat</span>
           </button>
           <button className="moreBtn">
-            <InfoOutlinedIcon /> <span>More</span>
+            <InfoOutlinedIcon /> <span>Daha Fazla Bilgi</span>
           </button>
         </div>
       </div>
