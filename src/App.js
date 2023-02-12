b3474e5dffafce442eb56e549e34dab77ef6f97d
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-12 20:32:36 +0300

    style: edited register page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import Home from "./pages/home/Home";
 import "./app.scss";
 import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
 
 function App() {
-  return <Watch />;
+  return <Register />;
 }
 
 export default App;
