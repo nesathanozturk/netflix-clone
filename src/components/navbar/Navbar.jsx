a44b78a8766344830d834340a4e3d13c165c5d06
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-12 21:16:46 +0300

    feat: added assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./navbar.scss";
+import logo from "../../assets/logo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
@@ -16,19 +17,16 @@ function Navbar() {
     <header className={isScrolled ? "navbar scrolled" : "navbar"}>
       <navbar>
         <div className="left">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-            alt="Netflix Logo"
-          />
-          <span>Home Page</span>
-          <span>Series</span>
-          <span>Movies</span>
-          <span>New and Popular</span>
-          <span>List</span>
+          <img src={logo} alt="Netflix Logo" />
+          <span>Ana Sayfa</span>
+          <span>Diziler</span>
+          <span>Filmler</span>
+          <span>Yeni Çıkanlar</span>
+          <span>Listem</span>
         </div>
         <div className="right">
           <SearchIcon className="icon" />
-          <span>Kıd</span>
+          <span>Ebeveyn</span>
           <NotificationsIcon className="icon" />
           <img
             src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
@@ -37,8 +35,8 @@ function Navbar() {
           <div className="profile_box">
             <ArrowDropDownIcon className="icon" />
             <div className="options">
-              <span>Settings</span>
-              <span>Log out</span>
+              <span>Ayarlar</span>
+              <span>Çıkış Yap</span>
             </div>
           </div>
         </div>
