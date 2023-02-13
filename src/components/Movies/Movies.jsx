10a2fb7eacd06820537d3bd8566ad25c6f2fe8d9
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-13 16:59:21 +0300

    feat: added slider

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,60 +1,68 @@
-import { useState, useRef } from "react";
 import { movies } from "../../assets/data/data";
 import CategoryTitle from "../CategoryTitle/CategoryTitle";
 import "../../styles/movies.scss";
 import MovieDetail from "../MovieDetail/MovieDetail";
 import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
 import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SampleNextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="control-btn" onClick={onClick}>
+      <button className="next">
+        <ArrowForwardIosOutlinedIcon />
+      </button>
+    </div>
+  );
+};
+const SamplePrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="control-btn" onClick={onClick}>
+      <button className="prev">
+        <ArrowBackIosOutlinedIcon />
+      </button>
+    </div>
+  );
+};
 
 function Movies({ title }) {
-  const [slideNumber, setSlideNumber] = useState(0);
-  const [isMoved, setIsMoved] = useState(false);
-
-  const listRef = useRef();
-
-  const handleClick = (direction) => {
-    setIsMoved(true);
-    let distance = listRef.current.getBoundingClientRect().x - 50;
-
-    if (direction === "left" && slideNumber > 0) {
-      setSlideNumber(slideNumber - 1);
-      listRef.current.style.transform = `translateX(${230 + distance}px)`;
-    }
-
-    if (direction === "right" && slideNumber < 9) {
-      setSlideNumber(slideNumber + 1);
-      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
-    }
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
 
   return (
-    <section>
-      <CategoryTitle title={title} />
-      <div className="wrapper">
-        {/* <ArrowBackIosOutlinedIcon
-          className="sliderArrow left"
-          onClick={() => handleClick("left")}
-          style={{ display: !isMoved && "none" }}
-        /> */}
-        {movies.map((movie, id) => (
-          <div key={movie.id} className="container" ref={listRef}>
-            <MovieDetail
-              title={movie.title}
-              smallPoster={movie.smallPoster}
-              description={movie.description}
-              minute={movie.minute}
-              limit={movie.limit}
-              year={movie.year}
-              genre={movie.genre}
-            />
-          </div>
-        ))}
-        {/* <ArrowForwardIosOutlinedIcon
-          className="sliderArrow right"
-          onClick={() => handleClick("right")}
-        /> */}
-      </div>
-    </section>
+    <main>
+      <section>
+        <CategoryTitle title={title} />
+        <div className="wrapper">
+          <Slider {...settings}>
+            {movies.map((movie, id) => (
+              <div key={movie.id} className="container">
+                <MovieDetail
+                  title={movie.title}
+                  smallPoster={movie.smallPoster}
+                  description={movie.description}
+                  minute={movie.minute}
+                  limit={movie.limit}
+                  year={movie.year}
+                  genre={movie.genre}
+                />
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </section>
+    </main>
   );
 }
 
