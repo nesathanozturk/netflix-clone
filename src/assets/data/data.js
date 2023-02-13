e7c36c2842d836de7266e567f349d9dd5a48bdb4
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-13 13:29:22 +0300

    feat: created data and imported photos

---
+++ src/assets/data/data.js
@@ -0,0 +1,246 @@
+import peakyTitlePoster from "../images/peaky-blinders-titlePoster.png";
+import peakyBg from "../images/peaky-blinders-bg.jpg";
+import peakySmallPoster from "../images/peaky-blinders-small-poster.jpg";
+import strangerThingsTitlePoster from "../images/stranger-things-titlePoster.png";
+import strangerThingsBg from "../images/stranger-things-bg.jpg";
+import strangerThingsSmallPoster from "../images/stranger-things-smallPoster.jpg";
+import bbTitlePoster from "../images/bb-titlePoster.png";
+import bbBg from "../images/bb-bg.jpg";
+import bbSmallPoster from "../images/bb-smallPoster.jpg";
+import dahmerTitlePoster from "../images/dahmer-titlePoster.png";
+import dahmerBg from "../images/dahmer-bg.jpg";
+import dahmerSmallPoster from "../images/dahmer-smallPoster.jpg";
+import interstallerTitlePoster from "../images/interstaller-titlePoster.png";
+import interstallerBg from "../images/interstaller-bg.jpg";
+import interstallerSmallPoster from "../images/interstaller-smallPoster.jpg";
+import zindanAdasiTitlePoster from "../images/zindan-adasi-titlePoster.png";
+import zindanAdasiBg from "../images/zindan-adasi-bg.jpg";
+import zindanAdasiSmallPoster from "../images/zindan-adasi-smallPoster.jpg";
+import trumanShowTitlePoster from "../images/truman-show-titlePoster.png";
+import trumanShowBg from "../images/truman-show-bg.jpg";
+import trumanShowSmallPoster from "../images/truman-show-smallPoster.jpg";
+import ww2TitlePoster from "../images/ww2-titlePoster.png";
+import ww2Bg from "../images/ww2-bg.jpg";
+import ww2SmallPoster from "../images/ww2-smallPoster.jpg";
+import godfatherTitlePoster from "../images/godfather-titlePoster.png";
+import godfatherBg from "../images/godfather-bg.jpg";
+import godfatherSmallPoster from "../images/godfather-smallPoster.webp";
+import narcosTitlePoster from "../images/narcos-titlePoster.png";
+import narcosBg from "../images/narcos-bg.jpg";
+import narcosSmallPoster from "../images/narcos-smallPoster.jpg";
+import darkTitlePoster from "../images/dark-titlePoster.png";
+import darkBg from "../images/dark-bg.jpg";
+import darkSmallPoster from "../images/dark-smallPoster.webp";
+import ragnarokTitlePoster from "../images/ragnarok-titlePoster.png";
+import ragnarokBg from "../images/ragnarok-bg.jpg";
+import ragnarokSmallPoster from "../images/ragnarok-smallPoster.jpg";
+import tenetTitlePoster from "../images/tenet-titlePoster.png";
+import tenetBg from "../images/tenet-bg.jpg";
+import tenetSmallPoster from "../images/tenet-smallPoster.jpg";
+import justiceLeagueTitlePoster from "../images/justice-league-titlePoster.png";
+import justiceLeagueBg from "../images/justice-league-bg.jpg";
+import justiceLeagueSmallPoster from "../images/justice-league-smallPoster.jpg";
+import fighterTitlePoster from "../images/fighter-titlePoster.png";
+import fighterBg from "../images/fighter-bg.jpg";
+import fighterSmallPoster from "../images/fighter-smallPoster.jpg";
+import darkKnightRisesTitlePoster from "../images/dark-knight-rises-titlePoster.png";
+import darkKnightRisesBg from "../images/dark-knight-rises-bg.jpg";
+import darkKnightRisesSmallPoster from "../images/dark-knight-rises-smallPoster.jpg";
+import darkKnightTitlePoster from "../images/dark-knight-titlePoster.png";
+import darkKnightBg from "../images/dark-knight-bg.jpg";
+import darkKnightSmallPoster from "../images/dark-knight-smallPoster.jpeg";
+import twdTitlePoster from "../images/twd-titlePoster.png";
+import twdBg from "../images/twd-bg.jpg";
+import twdSmallPoster from "../images/twd-smallPoster.jpg";
+
+export const movies = [
+  {
+    id: 1,
+    title: "Peaky Blinders",
+    titlePoster: peakyTitlePoster,
+    bigPoster: peakyBg,
+    smallPoster: peakySmallPoster,
+    description:
+      "1919 yılında İngiltere, Birmingham'daki bir suç çetesinin başında ne pahasına olursa olsun yükselmeyi kafasına koymuş, acımasız mafya babası Tommy Shelby vardır.",
+  },
+  {
+    id: 2,
+    title: "Stranger Things",
+    titlePoster: strangerThingsTitlePoster,
+    bigPoster: strangerThingsBg,
+    smallPoster: strangerThingsSmallPoster,
+    description:
+      "Ufak bir kasabada küçük bir çocuk kaybolunca, gizli deneyler, korkutucu doğaüstü güçler ve tuhaf bir küçük kızın da parçası olduğu bir gizem ortaya çıkar.",
+  },
+  {
+    id: 3,
+    title: "Breaking Bad",
+    titlePoster: bbTitlePoster,
+    bigPoster: bbBg,
+    smallPoster: bbSmallPoster,
+    description:
+      "Kanserden öleceğini öğrenen bir kimya öğretmeni, ailesinin geleceğini garanti altına almak için metamfetamin üretip satmak üzere eski bir öğrencisiyle kafa kafaya verir.",
+  },
+  {
+    id: 4,
+    title: "Canavar: Jeffrey Dahmer'in Hikayesi",
+    titlePoster: dahmerTitlePoster,
+    bigPoster: dahmerBg,
+    smallPoster: dahmerSmallPoster,
+    description:
+      "Hükümlü katil Jeffrey Dahmer, on yılı aşkın bir süre boyunca 17 genç erkeğin canına kıydı. Peki bunca zaman yakalanmamayı nasıl başardı?",
+  },
+  {
+    id: 5,
+    title: "Interstaller",
+    titlePoster: interstallerTitlePoster,
+    bigPoster: interstallerBg,
+    smallPoster: interstallerSmallPoster,
+    description:
+      "İnsanlık yok olmanın eşiğindeyken bir grup astronot, yaşanabilir başka bir gezegen bulmak için bir solucan deliğinden geçerek tehlikeli bir yolculuğa çıkar.",
+  },
+  {
+    id: 6,
+    title: "Zindan Adası",
+    titlePoster: zindanAdasiTitlePoster,
+    bigPoster: zindanAdasiBg,
+    smallPoster: zindanAdasiSmallPoster,
+    description:
+      "Bir psikiyatri merkezindeki kayıp hastayı soruşturan bir polis, aldatmacalarla örülü bir ağın ortasına düşer ve kendi gerçekliğini sorgulamaya başlar.",
+  },
+  {
+    id: 7,
+    title: "The Truman Show",
+    titlePoster: trumanShowTitlePoster,
+    bigPoster: trumanShowBg,
+    smallPoster: trumanShowSmallPoster,
+    description:
+      "Truman günün 24 saati yayınlanan ve tüm yaşantısını gözler önüne seren The Truman Show adlı televizyon programının yıldızıdır. Fakat kendisi bunun farkında değildir...",
+  },
+  {
+    id: 8,
+    title: "İkinci Dünya Savaşı: Renkli",
+    titlePoster: ww2TitlePoster,
+    bigPoster: ww2Bg,
+    smallPoster: ww2SmallPoster,
+    description:
+      "Renkli çekimler içeren bu etkileyici belgesel dizisinde, Pearl Harbor saldırısından D-Day çıkarmasına kadar 2. Dünya Savaşı'nın en önemli olayları ele alınıyor.",
+  },
+  {
+    id: 9,
+    title: "Baba",
+    titlePoster: godfatherTitlePoster,
+    bigPoster: godfatherBg,
+    smallPoster: godfatherSmallPoster,
+    description:
+      "New York'ta güçlü bir mafya babasının savaş kahramanı olan en küçük oğlu Michael Corleone, babasının suikasta hedef olmasının ardından aile işine girer.",
+  },
+  {
+    id: 10,
+    title: "Narcos",
+    titlePoster: narcosTitlePoster,
+    bigPoster: narcosBg,
+    smallPoster: narcosSmallPoster,
+    description:
+      "Bu gangster dizisinde, Kolombiya'nın şiddet dolu ve güçlü uyuşturucu kartellerinin gerçek hikayesini izleyeceksiniz.",
+  },
+  {
+    id: 11,
+    title: "Dark",
+    titlePoster: darkTitlePoster,
+    bigPoster: darkBg,
+    smallPoster: darkSmallPoster,
+    description:
+      "Bir çocuğun kaybolması, dört aileyi hummalı bir cevap arayışına iter. Bu arayışları, üç nesli kapsayan akıl almaz bir gizemi ortaya çıkarmalarına neden olur.",
+  },
+  {
+    id: 12,
+    title: "Ragnarok",
+    titlePoster: ragnarokTitlePoster,
+    bigPoster: ragnarokBg,
+    smallPoster: ragnarokSmallPoster,
+    description:
+      "Kirliliğin zehirlediği ve eriyen buzulların sarstığı bir Norveç kasabası, kıyameti yoğun biçimde hissetmektedir. Kadim bir kötülüğü alt etmek için bir efsane gereklidir.",
+  },
+  {
+    id: 13,
+    title: "Tenet",
+    titlePoster: tenetTitlePoster,
+    bigPoster: tenetBg,
+    smallPoster: tenetSmallPoster,
+    description:
+      "Gizli bir ajan, dünyanın sonunu getirebilecek bir zaman savaşının önüne geçmek için esrarengiz ve güçlü bir silahı ele geçirmeye çalışır.",
+  },
+  {
+    id: 14,
+    title: "Adalet Birliği",
+    titlePoster: justiceLeagueTitlePoster,
+    bigPoster: justiceLeagueBg,
+    smallPoster: justiceLeagueSmallPoster,
+    description:
+      "Batman, bugüne kadarki en zorlu düşmanlarını yenmek için Wonder Woman, Aquaman, Flash ve Cyborg gibi süper kahramanlardan oluşan bir ekip kurar.",
+  },
+  {
+    id: 15,
+    title: "Dövüşçü",
+    titlePoster: fighterTitlePoster,
+    bigPoster: fighterBg,
+    smallPoster: fighterSmallPoster,
+    description:
+      "Art arda gelen yenilgilerin ardından boksör, antrenörünün ve eskiden yetenekli bir dövüşçü olan üvey kardeşinin yardımıyla dövüş arzusunu yeniden keşfeder.",
+  },
+  {
+    id: 16,
+    title: "Batman: Kara Şövalye Yükseliyor",
+    titlePoster: darkKnightRisesTitlePoster,
+    bigPoster: darkKnightRisesBg,
+    smallPoster: darkKnightRisesSmallPoster,
+    description:
+      "Acımasız terörist Bane ve rengini belli etmeyen kedi kadın Selina Kyle, Bruce Wayne'i gizlendiği yerden çıkarak Gotham'ı kurtarmak için mücadele etmek zorunda bırakır.",
+  },
+  {
+    id: 17,
+    title: "Batman: Kara Şövalye",
+    titlePoster: darkKnightTitlePoster,
+    bigPoster: darkKnightBg,
+    smallPoster: darkKnightSmallPoster,
+    description:
+      "Batman, Teğmen Gordon ve Bölge Savcısı Harvey Dent, tüyler ürpertici makyajıyla Gotham şehrine korku saçan suç dehası Joker'e karşı mücadele eder.",
+  },
+  {
+    id: 18,
+    title: "The Walking Dead",
+    titlePoster: twdTitlePoster,
+    bigPoster: twdBg,
+    smallPoster: twdSmallPoster,
+    description:
+      "Zombi kıyametinin ardından hayatta kalanlar, birlikte verdikleri ölüm kalım mücadelesinde insanlığa karşı duydukları umuda tutunur.",
+  },
+  {
+    id: 19,
+    title: "Mindhunter",
+    titlePoster: "",
+    bigPoster: "",
+    smallPoster: "",
+    description:
+      "1970'lerin sonunda iki FBI ajanı, cinayet psikolojisini araştırarak ve tamamıyla gerçek canilerle endişe verici bir yakınlık kurarak suç biliminde yeni ufuklar açar.",
+  },
+  {
+    id: 20,
+    title: "El Camino: Bir Breaking Bad Filmi",
+    titlePoster: "",
+    bigPoster: "",
+    smallPoster: "",
+    description:
+      "Firari Jesse Pinkman, geçmişinden kaçmaya çalışır. 'Breaking Bad'in yaratıcısı Vince Gilligan'ın yazıp yönettiği filmin başrolünde Aaron Paul var.",
+  },
+  {
+    id: 21,
+    title: "Batı Cephesinde Yeni Bir Şey Yok",
+    titlePoster: "",
+    bigPoster: "",
+    smallPoster: "",
+    description:
+      "17 yaşındaki Paul, 1. Dünya Savaşı'nda Batı Cephesi'ne katılır. Siperlerdeki hayatın korkunç gerçekliği yüzüne çarpınca, ilk baştaki heyecanı çok geçmeden paramparça olur.",
+  },
+];
