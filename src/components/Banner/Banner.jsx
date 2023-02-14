5f399cba6252b35916edbfac0178048d680ae588
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-14 12:14:51 +0300

    feat: added trailer in banner page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,19 @@
 import BannerDetail from "../BannerDetail/BannerDetail";
 import { movies } from "../../assets/data/data";
 import "../../styles/banner.scss";
+import trailer from "../../assets/peaky-blinders-trailer.mp4";
 
 function Banner() {
   return (
     <div className="banner">
-      <img src={movies[0].bigPoster} alt="Background Poster" />
+      <video
+        id="video"
+        className="banner-video"
+        poster={movies[0].bigPoster}
+        src={trailer}
+        type="video/mp4"
+        controls
+      ></video>
       <BannerDetail movies={movies} />
     </div>
   );
