8d10c060b28da02bce391c607e70fd6125006a41
Neşathan Öztürk <107864503+nesathanozturk@users.noreply.github.com>
2023-02-15 13:20:30 +0300

    Update Main.jsx

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,12 +1,12 @@
 import useAuthContext from "../../hooks/use-auth-context";
-import Home from "../Home/Home";
+import Login from "../Login/Login.jsx";
 import User from "../User/User";
 import "../../styles/_reset.scss";
 
 function Main() {
   const { isLoggedIn } = useAuthContext();
 
-  return <>{isLoggedIn ? <User /> : <Home />}</>;
+  return <>{isLoggedIn ? <User /> : <Login />}</>;
 }
 
 export default Main;
